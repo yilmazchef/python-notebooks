f45cf75e6612d4b721a921725f52e30e3941197e
Yilmaz MUSTAFA <yilmaz.mustafa@intecbrussel.be>
2022-04-14 01:11:29 +0200

    course materials are now presented in different data formats such as .py, .docx, and .odt

---
+++ update_docs.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+
+import os
+import sys
+import json
+
+
+def list_files(filepath, filetype):
+    paths = []
+    for root, dirs, files in os.walk(filepath):
+        for file in files:
+            if file.lower().endswith(filetype.lower()):
+                paths.append(os.path.join(root, file))
+    return(paths)
+
+
+def to_py(ipynb_file_list):
+    print(f"{ipynb_file} to {md_file} conversion started...")
+    for file in ipynb_file_list:
+        code = json.load(open(file))
+        for cell in code['cells']:
+            if cell['cell_type'] == 'code':
+                print('# -------- code --------')
+                for line in cell['source']:
+                    print(line, end='')
+                print('\n')
+            elif cell['cell_type'] == 'markdown':
+                print('# -------- markdown --------')
+                for line in cell['source']:
+                    print("#", line, end='')
+                print('\n')
+
+    print(f"{ipynb_file} to {md_file} conversion complete...")
+
+
+def to_md(ipynb_file, md_file):
+    print(f"{ipynb_file} to {md_file} conversion started...")
+    cmdlet = f'jupyter nbconvert --to markdown {ipynb_file}'
+    print(cmdlet)
+    os.system(cmdlet)
+    print(f"{ipynb_file} to {md_file} conversion complete...")
+
+
+def to_docx(md_file, docx_file):
+    print(f"{md_file} to {docx_file} conversion started...")
+    cmdlet = f"pandoc {md_file} -f markdown -o {docx_file}"
+    print(cmdlet)
+    os.system(cmdlet)
+    print(f"{md_file} to {docx_file} conversion complete...")
+
+
+def to_odt(md_file, odt_file):
+    print(f"{md_file} to {odt_file} conversion started...")
+    cmdlet = f"pandoc -t odt {md_file} -o {odt_file}"
+    print(cmdlet)
+    os.system(cmdlet)
+    print(f"{md_file} to {odt_file} conversion complete...")
+
+
+ipynb_file_list = list_files(os.getcwd(), ".ipynb")
+
+for ipynb_file in ipynb_file_list:
+    md_file = ipynb_file.replace(".ipynb", ".md")
+    docx_file = ipynb_file.replace(".ipynb", ".docx")
+    odt_file = ipynb_file.replace(".ipynb", ".odt")
+
+    print("#" * 100)
+
+    to_md(ipynb_file, md_file)
+    to_docx(md_file, docx_file)
+
+    print("#" * 100)
+
+
+ipynb_file_list = list_files(os.getcwd(), ".ipynb")
