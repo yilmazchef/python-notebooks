5ea4ba1c93055eaccd4106ccda8590fee208bbb9
Yilmaz MUSTAFA <yilmaz.mustafa@intecbrussel.be>
2022-04-15 01:20:58 +0200

    data json design fixes..

--- folder_structure.py
+++ folder_structure.py
@@ -6,18 +6,20 @@ import json
 import fnmatch
 import re
 
+
 def path_to_dict(root_path):
-    
-    includes = ['*.md', '*.ipynb'] # for files only
-    excludes = ['.vscode', '.git', '__pycache__', 'Books', 'Code', 'Presentations'] # for dirs and files
+
+    includes = ['*.md', '*.ipynb']  # for files only
+    excludes = ['.vscode', '.git', '__pycache__', 'Books',
+                'Code', 'Presentations']  # for dirs and files
 
     # transform glob patterns to regular expressions
     includes = r'|'.join([fnmatch.translate(x) for x in includes])
     excludes = r'|'.join([fnmatch.translate(x) for x in excludes]) or r'$.'
-    
+
     for root, dirs, files in os.walk(root_path, topdown=True):
-        
-          # exclude dirs
+
+        # exclude dirs
         dirs[:] = [os.path.join(root, d) for d in dirs]
         dirs[:] = [d for d in dirs if not re.match(excludes, d)]
 
@@ -25,7 +27,7 @@ def path_to_dict(root_path):
         files = [os.path.join(root, f) for f in files]
         files = [f for f in files if not re.match(excludes, f)]
         files = [f for f in files if re.match(includes, f)]
-        
+
         tree = {
             "name": os.path.basename(root),
             "test": str(uuid4()),
@@ -37,22 +39,27 @@ def path_to_dict(root_path):
         tree["child"].extend(
             [path_to_dict(os.path.join(root, d)) for d in dirs]
         )
-        
+
         tree["child"].extend(
             [{
                 "name": os.path.basename(os.path.join(root, f)),
                 "test": str(uuid4()),
                 "icon": pathlib.Path(os.path.join(root, f)).suffix,
-                "link": os.path.join(root, f),
+                "link": str(os.path.join(root, f)),
                 "type": "file"
             } for f in files]
         )
 
+        tree = {k: v for k, v in tree.items() if v}
+
         return tree
-    
-def path_to_json_file(root_path, dict_path): 
-    with open(root_path, 'w') as file:
-        json_string = json.dumps(dict_path, default=lambda o: o.__dict__, sort_keys=True, indent=2)
+
+
+def dict_to_json(dict_path:dict, json_path:str):
+    with open(json_path, 'w') as file:
+        
+        json_string = json.dumps(
+            dict_path, default=lambda o: o.__dict__, sort_keys=True, indent=2)
         file.write(json_string)
 
 
@@ -64,4 +71,3 @@ def notebook_file_paths(folder_path, file_type):
                 paths.append(os.path.join(root, file))
 
     return paths
-
--- table_of_contents.py
+++ table_of_contents.py
@@ -4,7 +4,7 @@ import json
 import logging
 import time
 from uuid import uuid4
-from folder_structure import path_to_dict, path_to_json_file, notebook_file_paths
+from folder_structure import path_to_dict, dict_to_json, notebook_file_paths
 
 logging.basicConfig(filename='logs.json', filemode='a+', level=logging.DEBUG)
 
@@ -17,9 +17,11 @@ if __name__ == "__main__":
 
     os.chdir(cwd)
 
+    
+
     tree = path_to_dict(cwd)
 
-    path_to_json_file(cwd + os.path.sep + "data.json", tree)
+    dict_to_json(tree, cwd + os.path.sep + "data.json")
 
     end = time.time()
     print(end - start)
